ea7e85c29197ad752d95f9e54773e35479b33d51
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2020-08-24 00:06:48 +0900

    commit

--- vue-tabs.js
+++ vue-tabs.js
@@ -13,10 +13,33 @@
 'use strict'
 
 !function() {
+
+  var codes = {
+
+    Z:  { name: '-',  notes: [ 0, 0, 0, 0 ] },
+
+    C:  { name: 'C',  notes: [ 0, 0, 0, 3 ] },
+    C7: { name: 'C7', notes: [ 0, 0, 0, 1 ] },
+
+    Dm: { name: 'Dm', notes: [ 2, 2, 1, 0 ] },
+    D7: { name: 'D7', notes: [ 2, 0, 2, 0 ] },
+
+    Em: { name: 'Em', notes: [ 0, 4, 3, 2 ] },
+    E7: { name: 'E7', notes: [ 1, 2, 0, 2 ] },
+
+    F:  { name: 'F',  notes: [ 2, 0, 1, 0 ] },
+    Fm: { name: 'Fm', notes: [ 1, 0, 1, 3 ] },
+
+    G7: { name: 'G7', notes: [ 0, 2, 1, 2 ] },
+
+    Am: { name: 'Am', notes: [ 2, 0, 0, 0 ] },
+    A7: { name: 'A7', notes: [ 0, 1, 0, 0 ] }
+  };
+
   var components = {
     tab: {
       template: '<span style="display:inline-block;">' +
-        '<label>{{data.name}}</label><br/>' +
+        '<label>{{params.name}}</label><br/>' +
         '<svg xmlns="http://www.w3.org/2000/svg"' +
           ' :width="sWidth" :height="sHeight"' +
           ' :view-box.camel="sViewBox">' +
@@ -33,10 +56,8 @@
             ' :style="point.open? openNoteStyle : noteStyle"></circle>' +
         '</svg></span>',
       props: {
-        data: { type: Object, default: function() { return {
-          name: 'C',
-          notes: [ 0, 0, 0, 3 ]
-        }; } }
+        code: { type: String, default: '' },
+        data: { type: Object, default: function() { return codes['Z']; } }
       },
       computed: {
         sWidth: function() { return this.params.width + 'px'; },
@@ -46,11 +67,12 @@
         },
         params: function() {
 
-          var notes = this.data.notes;
-          var numFlets = 5;
+          var data = (this.code? codes[this.code] : this.data) || codes['Z'];
+          var notes = data.notes;
+          var numFlets = 4;
           var hGap = 15;
           var vGap = 6;
-          var width = numFlets * 10 + hGap * 2;
+          var width = (numFlets + 1) * 10 + hGap * 2;
           var height = notes.length * 10 + vGap * 2;
           var noteRadius = 4;
 
@@ -63,21 +85,21 @@
           });
 
           var vLines = [];
-          for (var i = 0; i < numFlets; i += 1) {
-            var x = hGap + (w / (numFlets - 1) ) * i;
+          for (var i = 0; i <= numFlets; i += 1) {
+            var x = hGap + w / numFlets * i;
             vLines.push('M' + x + ' ' + vGap + 'L' + x + ' ' + (height - vGap) );
           }
 
           var points = notes.map(function(note, i) {
             var n = notes[notes.length - i - 1];
             var open = n == 0;
-            var x = hGap + (w / (numFlets - 1) ) * (n - 0.5);
+            var x = hGap + w / numFlets * (n - 0.5);
             var y = vGap + (h / (notes.length - 1) ) * i;
             return { x: x, y: y, open: open };
           });
 
           return {
-            numFlets: numFlets,
+            name: data.name,
             width: width,
             height: height,
             hGap: hGap,
@@ -100,11 +122,6 @@
         openNoteStyle: function() {
           return 'stroke: #000; fill: none;';
         }
-      },
-      data: function() {
-        return {
-
-        };
       }
     }
   };
